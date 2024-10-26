78ce01ab8c891927c3d347d6ec6f02d5105a32fe
Vladislav Zdobnov <6la9isla6@gmail.com>
2024-10-26 19:59:15 +0300

    add hello_world.cpp file

---
+++ hello_world.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <climits>
+
+int main() {
+    std::cout << sizeof(short) << std::endl;
+    std::cout << sizeof(int) << std::endl;
+    std::cout << sizeof(long) << std::endl;
+    std::cout << sizeof(long long) << std::endl;
+    std::cout << CHAR_BIT << std::endl;
+    std::cout << 9 << std::endl;
+
+    return 0;
+}
