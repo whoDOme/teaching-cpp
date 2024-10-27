705c2f33376091117adfe98ca82a8afc9a917a77
Vladislav Zdobnov <6la9isla6@gmail.com>
2024-10-27 15:10:39 +0300

    Revert "deleted cout nine"
    This reverts commit 2f0a45316782a3229a9513d9ec7ac07bc41243a3.

--- hello_world.cpp
+++ hello_world.cpp
@@ -7,6 +7,7 @@ int main() {
     std::cout << sizeof(long) << std::endl;
     std::cout << sizeof(long long) << std::endl;
     std::cout << CHAR_BIT << std::endl;
+    std::cout << 9 << std::endl;
 
     return 0;
 }
