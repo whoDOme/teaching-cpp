9f5ae14f591978bbacca01af90c466f8a5b21e8c
Vladislav Zdobnov <6la9isla6@gmail.com>
2024-11-22 10:13:18 +0300

    vector, insert metod and some iterators

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,14 +1,36 @@
 #include <iostream>
-#include <climits>
 
 int main() {
+    std::vector<int> vec;
+    vec.reserve(9);
+    std::vector<int> other = {1, 2, 3, 4, 5};
+
+    vec.push_back(93);
+    auto it = vec.insert(vec.begin() + 1, 15);
+    vec.insert(vec.end(), 3, 34);
+    vec.insert(vec.begin() + 3, other.begin() + 1, other.end());
     {
-        if (int a = rand(), b = 100; a > b) {
-            std::cout << a << b << "a" << std::endl;
-        }
-        else {
-            std::cout << a << b << "b" << std::endl;
-        }
+        it = vec.end();
+        --it;
+        std::cout << *it << std::endl;
+        --it;
+        std::cout << *it << std::endl;
+        --it;
+        std::cout << *it << std::endl;
+    }
+    {
+        it = vec.begin();
+        std::cout << *it << std::endl;
+        ++it;
+        std::cout << *it << std::endl;
+        ++it;
+        std::cout << *it << std::endl;
+
+    }
+
+    std::cout << vec.size() << std::endl;
+    for (int value : vec) {
+        std::cout << value << " ";
     }
 
     return 0;
