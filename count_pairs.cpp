f3accc8572542067839a88b37de4b32dbbd79410
Vladislav Zdobnov <6la9isla6@gmail.com>
2025-01-16 14:39:20 +0300

    The task "count_pairs" is completed

---
+++ count_pairs.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+#include <map>
+
+uint16_t CountPairs(const std::vector<int16_t>& values, int16_t k) {
+    std::map<int16_t, uint16_t> values_count;
+    for (const auto elem : values) {
+        ++values_count[elem];
+    }
+
+    uint16_t count_pairs = 0;
+    for (const auto& [key, value] : values_count) {
+        if (key >= k / 2) {
+            count_pairs += (value * (value - 1)) / 2;
+            break;
+        }
+        if (values_count.count(k - key)) {
+            count_pairs += value * values_count[k - key];
+        }
+    }
+
+    return count_pairs;
+}
+
+int main() {
+    size_t values_size = 0;
+    std::cin >> values_size;
+    std::vector<int16_t> values(values_size);
+    for (auto& elem : values) {
+        std::cin >> elem;
+    }
+
+    int16_t k = 0;
+    std::cin >> k;
+
+    std::cout << CountPairs(values, k) << std::endl;
+
+    return 0;
+}
